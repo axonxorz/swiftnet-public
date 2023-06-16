d1b62c083227eb41a3be0e28a720547e7e5b7c73
Younes Bouchbouk <younesbouchbouk.py@gmail.com>
2023-06-16 14:04:06 +0100

    add Head tag with desxription and keywords

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,82 @@ import { Toaster } from "react-hot-toast";
 import Landing from "./landing/page";
 import Script from "next/script";
 
+export const metadata = {
+  title: "Swift Net",
+  description:
+    "Get connected with Swift-Net.ca, the leading home Wi-Fi internet service provider in Alberta & Saskatchewan, providing high-speed wireless internet service in Lloydminster, Cold Lake, North Battleford, and more rural communities. Sign up today! Experience the difference with Swift-Net.ca's reliable internet service, designed specifically for rural communities in Alberta and Saskatchewan. Sign up today and enjoy fast and dependable home wi-fi for you and your family!",
+  keywords: [
+    "Swift-Net.ca",
+    "rural Canadians",
+    "high-speed connectivity",
+    "unlimited internet service",
+    "data caps",
+    "overages",
+    "throttling",
+    "fixed monthly price",
+    "wireless internet service",
+    "exceptional browsing experience",
+    "peace of mind",
+    "freedom",
+    "surf",
+    "stream",
+    "game",
+    "data limits",
+    "unrestricted internet access",
+    "online activities",
+    "connectivity",
+    "unlimited data usage",
+    "streaming",
+    "browsing",
+    "downloading",
+    "consistent speeds",
+    "peak hours",
+    "data threshold",
+    "no surprise charges",
+    "fixed monthly pricing",
+    "hidden fees",
+    "affordable plans",
+    "customer satisfaction",
+    "home Wi-Fi internet service",
+    "high-speed home Wi-Fi",
+    "limitations",
+    "restrictions",
+    "Connect",
+    "truly unlimited internet",
+    "rural Alberta",
+    "Saskatchewan",
+    "High-Speed Internet",
+    "fast and reliable",
+    "streaming",
+    "browsing",
+    "gaming",
+    "buffering",
+    "delays",
+    "Rural Focus",
+    "dedicated",
+    "serving",
+    "communities",
+    "Easy Sign-Up Process",
+    "home address",
+    "signup form",
+    "Local Support",
+    "friendly",
+    "knowledgeable staff",
+    "Alberta",
+    "Saskatchewan",
+    "internet service",
+    "rural communities",
+    "Affordable Pricing",
+    "competitive pricing",
+    "variety of plans",
+    "budget",
+    "Get Connected",
+    "satisfied customers",
+    "high-speed wireless internet service",
+    "signup form",
+  ],
+};
+
 export default function Home() {
   return (
     <>
@@ -16,7 +92,6 @@ export default function Home() {
         src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v17.0"
         nonce="VhNMcz7M"
       />
-
 
       <Toaster />
 
