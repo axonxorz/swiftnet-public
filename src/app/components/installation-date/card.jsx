bee03db9d2350de99826cbb66985135d9c839d7b
Younes Bouchbouk <younesbouchbouk.py@gmail.com>
2023-07-06 18:00:54 +0100

    feat : hide basic plan and display it when slected

---
+++ src/app/components/installation-date/card.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+
+const Card = ({ setSelectedPlan, plan, selectedPlan }) => {
+  return (
+    <div
+      onClick={() => setSelectedPlan(plan)}
+      className={`${
+        selectedPlan.id === plan.id
+          ? "border-primary border-[1px] bg-[#F1FAFF] "
+          : "border-gray-400/70 border-[1px] "
+      }col-span-1 cursor-pointer hover:border-primary relative min-h-[260px] rounded-lg  flex flex-col  space-y-3 py-4`}
+      key={plan.id}
+    >
+      {plan.id === 2 && (
+        <div className="absolute bg-primary top-0 left-0 px-4 py-[2px] text-white  rounded-tl-lg rounded-br-lg  ">
+          <p>Recommended</p>
+        </div>
+      )}
+      {selectedPlan.id !== plan.id ? (
+        <div className="absolute h-[20px] w-[20px] border-[1px] border-black rounded-full top-5 right-5"></div>
+      ) : (
+        <div className="absolute h-[20px] w-[20px]  rounded-full top-5 right-5">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M12 1C5.92487 1 1 5.92487 1 12C1 18.0751 5.92487 23 12 23C18.0751 23 23 18.0751 23 12C23 5.92487 18.0751 1 12 1ZM17.2071 9.70711C17.5976 9.31658 17.5976 8.68342 17.2071 8.29289C16.8166 7.90237 16.1834 7.90237 15.7929 8.29289L10.5 13.5858L8.20711 11.2929C7.81658 10.9024 7.18342 10.9024 6.79289 11.2929C6.40237 11.6834 6.40237 12.3166 6.79289 12.7071L9.79289 15.7071C10.1834 16.0976 10.8166 16.0976 11.2071 15.7071L17.2071 9.70711Z"
+              fill="#05649C"
+            />
+          </svg>
+        </div>
+      )}
+
+      <div className="text-center w-full h-[80px] flex gap-1 flex-col items-center justify-center">
+        <p className="font-bold text-[18px]">{plan.title}</p>
+
+        <p className="border-[1px] rounded-md bg-white border-primary text-[12px] px-3 py-[1px]">
+          Unlimited
+        </p>
+      </div>
+
+      <div className="text-center ">
+        <p className="text-primary text-[24px] font-bold text-center  ">
+          ${plan.price}
+        </p>
+        <p className="text-[12px] text-[#4B5563] -mt-2">/month</p>
+      </div>
+
+      <div className="w-full flex flex-col items-start gap-3 px-6">
+        {plan.include.map((itm) => {
+          return (
+            <div key={itm} className="flex items-center justify-center gap-3">
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g clip-path="url(#clip0_515_2708)">
+                  <path
+                    d="M14.6662 7.39053V8.00386C14.6654 9.44146 14.1999 10.8403 13.3391 11.9917C12.4784 13.1431 11.2685 13.9855 9.88985 14.3931C8.51124 14.8007 7.0378 14.7517 5.68929 14.2535C4.34077 13.7553 3.18943 12.8346 2.40697 11.6285C1.62452 10.4225 1.25287 8.99589 1.34746 7.5614C1.44205 6.12691 1.99781 4.76143 2.93185 3.6686C3.86589 2.57578 5.12817 1.81416 6.53043 1.49734C7.93269 1.18051 9.39979 1.32546 10.7129 1.91057M14.6662 2.66675L7.99962 9.34003L5.99964 7.34004"
+                    stroke="#05649C"
+                    stroke-width="1.33332"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_515_2708">
+                    <rect width="15.9999" height="15.9999" fill="white" />
+                  </clipPath>
+                </defs>
+              </svg>
+
+              <p className="text-small text-black">{itm}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/app/components/installation-date/plans.jsx
@@ -0,0 +1,112 @@
+import styles from "@/app/styles/styles";
+import React, { useState } from "react";
+import Card from "./card";
+
+export const planesList = [
+  {
+    id: 0,
+    title: "Basic",
+    unlimited: true,
+    price: 69.95,
+    include: ["browser", "Email", "Facebook"],
+  },
+  {
+    id: 1,
+    title: "Streaming",
+    unlimited: true,
+    price: 77.95,
+    include: ["Youtube", "Netflix", "Live Sport"],
+  },
+  {
+    id: 2,
+    title: "family",
+    unlimited: true,
+    price: 84.95,
+    include: ["Multiple users", "Online learning", "TikTok"],
+  },
+  {
+    id: 3,
+    title: "Gamer",
+    unlimited: true,
+    price: 104.95,
+    include: ["PC gaming", "Xbox", "PlayStation", "Nintendo"],
+  },
+  {
+    id: 4,
+    title: "Ultimate",
+    unlimited: true,
+    price: 129.95,
+    include: ["Competitive gamers", "Smart TVs", "Large households"],
+  },
+  {
+    id: 5,
+    title: "Business",
+    unlimited: true,
+    price: 179,
+    include: ["Work from home", "Zoom", "VPN"],
+  },
+  {
+    id: 6,
+    title: "Dedicated",
+    unlimited: true,
+    price: 499,
+    include: ["Dedicated link"],
+  },
+];
+const Plans = ({ selectedPlan, setSelectedPlan }) => {
+  const [hideBasic, sethideBasic] = useState(true);
+  return (
+    <div className="w-full text-start ">
+      <label className={`${styles.paragraph} text-[#4B5563]`}>
+        Preferred Plan
+      </label>
+
+      {!hideBasic ? (
+        <div className="grid grid-cols-2 md:grid-cols-4  gap-4">
+          {planesList.map((plan) => {
+            return (
+              <Card
+                plan={plan}
+                selectedPlan={selectedPlan}
+                setSelectedPlan={setSelectedPlan}
+                key={plan.id}
+              />
+            );
+          })}
+        </div>
+      ) : (
+        <div className="flex justify-start items-start gap-4">
+          <div
+            className="w-[50px] cursor-pointer hover:bg-[#F1FAFF] min-h-[330px] rounded-lg  p-4 border-[1px] flex items-center justify-center flex-col gap-4 "
+            onClick={() => {
+              sethideBasic(false);
+              setSelectedPlan(planesList[0]);
+            }}
+          >
+            <p className="font-bold">B</p>
+            <p className="font-bold">A</p>
+            <p className="font-bold">S</p>
+            <p className="font-bold">I</p>
+            <p className="font-bold">C</p>
+          </div>
+          <div className="grid grid-cols-2 md:grid-cols-4  gap-4">
+            {planesList
+              .filter((byId) => byId.id !== 0)
+              .map((plan) => {
+                return (
+                  <Card
+                    plan={plan}
+                    selectedPlan={selectedPlan}
+                    setSelectedPlan={setSelectedPlan}
+                    key={plan.id}
+                  />
+                );
+              })}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Plans;
