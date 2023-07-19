37387246839d8a07db8f8523430deb71a7d0e3bd
Younes Bouchbouk <younesbouchbouk.py@gmail.com>
2023-07-19 13:15:45 +0100

    send user to sign up when clicking on Get started

--- src/app/components/CommonLayers.jsx
+++ src/app/components/CommonLayers.jsx
@@ -3,12 +3,13 @@ import style from "../styles/styles.module.css";
 import styles from "../styles/styles";
 import Image from "next/image";
 import network from "../../assets/network.png";
+import Link from "next/link";
 
 const CommonLayers = (props) => {
   return (
     <div className="pt-20">
       <div className={`${props.imgUrl} h-[50vh] md:h-[70vh]`}></div>
-      <div className={`${styles.width}`}>
+      <div className={`${styles.width} -z-20`}>
         <div className="md:w-3/5 m-auto">
           <h2 className={`${styles.heading} mt-5 mb-3`}>{props.heading}</h2>
           <p className={`${styles.paragraph} mb-7`}>{props.description}</p>
@@ -36,7 +37,7 @@ const CommonLayers = (props) => {
         </div>
       </div>
 
-      <div className="md:flex mb-14 md:mb-24">
+      <div className="md:flex mb-14 md:mb-24 z-[999]">
         <div
           className={`basis-1/2 bg-primary md:pt-[230px] relative md:top-[-120px] -z-20 px-4 md:px-0 md:pl-[7.5%] py-10 md:py-0`}
         >
@@ -47,9 +48,13 @@ const CommonLayers = (props) => {
           <p className={`${styles.paragraph} text-white mb-7 pr-10`}>
             {props.paragraph2 && props.paragraph2}
           </p>
-          <button className=" text-white font-medium text-base border-[1px] border-solid border-white px-4 py-2 rounded-md">
-            Get Started
-          </button>
+          <div className="mr-6 w-full flex">
+            <Link href={"/sign-up"}>
+              <button className=" text-white  w-[130px] cursor-pointer  font-medium text-base border-[1px] border-solid border-white px-4 py-2 rounded-md">
+                Get Started
+              </button>
+            </Link>
+          </div>
           <div className="absolute bottom-0 left-0">
             <Image src={network} alt="/" unoptimized={true} className="" />
           </div>
