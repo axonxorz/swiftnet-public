1d7717cd622e6a20365ecf9638858b9d9e6c9623
Younes Bouchbouk <younesbouchbouk.py@gmail.com>
2023-07-19 20:20:00 +0100

    feat : make some changes on the autocomplete input

--- src/app/components/home/AutoCompleteInput.jsx
+++ src/app/components/home/AutoCompleteInput.jsx
@@ -57,23 +57,45 @@ const AutoCompleteInput = ({ setUserLocation, place }) => {
   if (place === "home") {
     return (
       <>
-        <div className="w-full relative">
+        <div className="w-full relative bg-white  p-1 md:p-0 rounded-md">
           <input
             disabled={loading}
             type="text"
             placeholder={loading ? "please wait ...." : "Enter Your address"}
             ref={inputRef}
-            className="block w-full rounded-md border-0 py-2 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            className="hidden md:block w-full rounded-md border-0 border-none py-2 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             onKeyPress={(event) => {
               if (event.key === "Enter") {
                 handleChackAvabilty();
               }
             }}
           />
+
+          <textarea
+            disabled={loading}
+            rows={2}
+            placeholder={loading ? "please wait ...." : "Enter Your address"}
+            ref={inputRef}
+            className="md:hidden block w-full rounded-md border-0 py-2 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            onKeyPress={(event) => {
+              if (event.key === "Enter") {
+                handleChackAvabilty();
+              }
+            }}
+          />
+
           <button
             type="submit"
             onClick={() => handleChackAvabilty()}
-            className={`bg-primary border-none rounded-md ${styles.paragraph} text-white px-4 py-2 absolute top-0  md:bottom-[50%] right-0   h-full md:w-auto`}
+            className={`bg-primary border-none rounded-md ${styles.paragraph}  text-white px-4 py-2 hidden absolute md:flex top-0  md:bottom-[50%] right-0   h-full `}
+          >
+            Check availability
+          </button>
+
+          <button
+            type="submit"
+            onClick={() => handleChackAvabilty()}
+            className={`bg-primary border-none rounded-md ${styles.paragraph}  text-white px-4 py-2  md:hidden top-0     w-full `}
           >
             Check availability
           </button>
