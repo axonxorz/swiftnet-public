12eea8e09ed948109db491839da1bfc53157c08f
Younes Bouchbouk <younesbouchbouk.py@gmail.com>
2023-08-09 13:51:13 +0100

    change jivochat script

--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -3,7 +3,7 @@ import FAQ from "@/app/components/faqs/index";
 import "@/app/styles/custom.css";
 
 export const metadata = {
-  title: "AB/SK internet provider outage FAQ by Swift-Net.ca",
+  title: "AB/SK internet provider outage [SIGN-UP NOW] for Swift-Net.ca",
   description:
     "Frustrated by MCSnet outages in Lloydminster, Cold Lake, Bonnyville, Lac La Biche, Macklin, and Athabasca? Is Access Communications offline in North Battleford, Kindersley and Meadow Lake? Tired of frequent Sasktel outages in Lloydminster North Battleford, and Meadow Lake?",
 };
