a75523986ba439ad4b0bf3566fc888850857f574
Brendan Zerr <bzerr@swift-net.ca>
2023-10-05 17:24:24 -0600

    Update plan definitions

--- src/app/components/installation-date/plans.jsx
+++ src/app/components/installation-date/plans.jsx
@@ -6,60 +6,28 @@ import BasicPlan from "./basic-plan-card";
 export const plansList = [
   {
     id: 0,
-    title: "Basic",
+    title: "LightSpeed 25/5",
     unlimited: true,
     price: 79.95,
-    include: ["Browser", "Email", "Facebook"],
-    bg: "",
-  },
-  {
-    id: 1,
-    title: "Streaming",
-    unlimited: true,
-    price: 94.95,
     include: ["Youtube", "Netflix", "Live Sports"],
     bg: "",
   },
   {
-    id: 2,
-    title: "Family",
+    id: 1,
+    title: "LightSpeed 50/10",
     unlimited: true,
-    price: 99.95,
+    price: 89.95,
     include: ["Multiple users", "Online learning", "TikTok"],
     bg: "",
   },
   {
-    id: 3,
-    title: "Gamer",
+    id: 2,
+    title: "LightSpeed 75/15",
     unlimited: true,
-    price: 119.95,
+    price: 99.95,
     include: ["PC gaming", "Xbox", "PlayStation", "Nintendo"],
     bg: "",
-  },
-  {
-    id: 4,
-    title: "Ultimate",
-    unlimited: true,
-    price: 149.95,
-    include: ["Competitive gamers", "Smart TVs", "Large households"],
-    bg: "bg-[#FAF3FF]",
-  },
-  {
-    id: 5,
-    title: "Business",
-    unlimited: true,
-    price: 179,
-    include: ["Work from home", "Zoom", "VPN"],
-    bg: "bg-[#ECFDF5]",
-  },
-  {
-    id: 6,
-    title: "Dedicated",
-    unlimited: true,
-    price: 499,
-    include: ["Dedicated link", "Managed internet"],
-    bg: "bg-[#FEF2F2]",
-  },
+  }
 ];
 const Plans = ({ selectedPlan, setSelectedPlan }) => {
   return (
@@ -68,16 +36,9 @@ const Plans = ({ selectedPlan, setSelectedPlan }) => {
         Preferred Plan
       </label>
 
-      <BasicPlan
-        selectedPlan={selectedPlan}
-        setSelectedPlan={setSelectedPlan}
-        key={"basic plan"}
-      />
-
       <div className="flex justify-start items-start gap-4 ">
         <div className="grid grid-cols-2 md:grid-cols-3  gap-4 w-full">
           {plansList
-            .filter((byId) => byId.id !== 0)
             .map((plan) => {
               return (
                 <Card
