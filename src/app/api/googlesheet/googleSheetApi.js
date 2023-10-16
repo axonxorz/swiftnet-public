b9cfd98cd0ca3045adde14dc2e3abec885c4f9b1
Brendan Zerr <bzerr@swift-net.ca>
2023-10-16 14:10:31 -0600

    Prevent build failure if GOOGLE spreadsheet APIs are not specified

--- src/app/api/googlesheet/googleSheetApi.js
+++ src/app/api/googlesheet/googleSheetApi.js
@@ -1,8 +1,8 @@
 import { google } from "googleapis";
 
 const credentials = {
-  client_email: process.env.GOOGLE_CLIENT_EMAIL,
-  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env?.GOOGLE_CLIENT_EMAIL || '',
+  private_key: (process.env?.GOOGLE_PRIVATE_KEY || '').replace(/\\n/g, "\n"),
 };
 
 const auth = new google.auth.GoogleAuth({
