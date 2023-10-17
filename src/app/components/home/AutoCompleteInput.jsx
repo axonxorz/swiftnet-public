767f319936c5e4d311dba0189e8fb70337543e81
Brendan Zerr <bzerr@swift-net.ca>
2023-10-17 11:32:31 -0600

    Simplify AutoCompleteInput

--- src/app/components/home/AutoCompleteInput.jsx
+++ src/app/components/home/AutoCompleteInput.jsx
@@ -4,125 +4,104 @@ import React from "react";
 import { useRef, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
-const AutoCompleteInput = ({ setUserLocation, place }) => {
-  const router = useRouter();
-  const autoCompleteRef = useRef();
-  const inputRef = useRef();
-  const [loading, setLoading] = useState(false);
-  const [placeObj, setPlaceObj] = useState({});
-  // const setAddress = ipAddressStore((state) => state.setAddress);
+import loader from '@/lib/gmaps'
+import { swiftAutocompleteOptions } from "@/lib/gis";
+import { useUserLocationStore } from "@/store";
+import { isNil } from "lodash-es";
 
-  const options = {
-    fields: ["address_components", "geometry", "icon", "name"],
-  };
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      autoCompleteRef.current = new window.google.maps.places.Autocomplete(
-        inputRef.current,
-        options
-      );
+const AutoCompleteInput = ({place}) => {
+    const router = useRouter();
+    const autoCompleteRef = useRef();
+    const inputRef = useRef();
+    const locationStore = useUserLocationStore();
 
-      autoCompleteRef?.current?.addListener("place_changed", async function () {
-        const place = await autoCompleteRef.current.getPlace();
-        if (place) {
-          setPlaceObj(place);
+    const localOptions = {
+        fields: ["address_components", "geometry", "icon", "name"],
+    };
+    useEffect(() => {
+        loader.load().then(async (google) => {
+            const autocompleter = new google.maps.places.Autocomplete(
+                inputRef.current,
+                Object.assign({}, swiftAutocompleteOptions, localOptions)
+            );
+            autoCompleteRef.current = autocompleter;
+            autocompleter.addListener('place_changed', async () => {
+                const place = await autocompleter.getPlace();
+                locationStore.setAddress(inputRef.current.value);
+                if(place) {
+                    locationStore.setCoordinates(
+                        place.geometry?.location?.lat() || null,
+                        place.geometry?.location?.lng() || null
+                    )
+                }
+            })
+        })
+    }, []);
+
+    const checkAvailability = () => {
+        const address = locationStore.address;
+        const {lat, lng} = locationStore;
+        let route = '/map';
+        let search = new URLSearchParams();
+        search.set('address', locationStore.address);
+        if(lat !== null && lng !== null) {
+            search.set('lat', lat);
+            search.set('lng', lng);
         }
-      });
-      setLoading(false);
-    }, 100);
-  }, []);
+        router.push(`${route}?${search}`);
+    };
 
-  useEffect(() => {
-    setUserLocation &&
-      setUserLocation({
-        lat: placeObj.geometry?.location?.lat(),
-        lng: placeObj.geometry?.location?.lng(),
-      });
-  }, [placeObj]);
-
-  const checkAvailability = async () => {
-    if (
-      typeof placeObj.geometry?.location?.lat() === "undefined" ||
-      typeof placeObj.geometry?.location?.lng() === "undefined"
-    ) {
-      router.push(`/map?fullAddress=${inputRef.current.value}`);
-    } else {
-      router.push(
-        `/map?lat=${placeObj.geometry?.location?.lat()}&lng=${placeObj.geometry?.location?.lng()}&fullAddress=${
-          inputRef.current.value
-        }`
-      );
+    if(locationStore.address || (!isNil(locationStore.lat) && !isNil(locationStore.lng))) {
+        checkAvailability();
     }
-  };
 
-  if (place === "home") {
-    return (
-      <>
-        <div className="w-full relative bg-white  p-1 md:p-0 rounded-md">
-          <input
-            disabled={loading}
-            placeholder={loading ? "please wait ...." : "Enter Your address"}
-            ref={inputRef}
-            className=" block min-h-[30px] w-full rounded-md border-0 py-2 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            onKeyPress={(event) => {
-              if (event.key === "Enter") {
-                checkAvailability();
-              }
-            }}
-          />
+    if (place === "home") {
+        return (
+            <>
+                <div className="w-full relative bg-white  p-1 md:p-0 rounded-md">
+                    <input
+                        placeholder="Enter Your address"
+                        ref={inputRef}
+                        className=" block min-h-[30px] w-full rounded-md border-0 py-2 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
 
-          <button
-            type="submit"
-            onClick={() => checkAvailability()}
-            className={`bg-primary border-none rounded-md ${styles.paragraph}   text-white px-4 py-2 hidden absolute md:flex  items-center justify-center top-0  md:bottom-[50%] right-0   h-full `}
-          >
-            Check availability
-          </button>
+                    <button
+                        type="submit"
+                        onClick={() => checkAvailability()}
+                        className={`bg-primary border-none rounded-md ${styles.paragraph}   text-white px-4 py-2 absolute md:flex  items-center justify-center top-0  md:bottom-[50%] right-0   h-full `}
+                    >
+                        Check Availability
+                    </button>
+                </div>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <div className="w-full relative">
+                    <label htmlFor="" className={`text-xs text-gray-500`}>
+                        Address
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="Enter Your address"
+                        ref={inputRef}
+                        className="block w-full rounded-md border-0 py-2 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                </div>
 
-          <button
-            type="submit"
-            onClick={() => checkAvailability()}
-            className={`bg-primary border-none rounded-md ${styles.paragraph}  text-white px-4 py-2  md:hidden top-0     w-full `}
-          >
-            Check availability
-          </button>
-        </div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <div className="w-full relative">
-          <label htmlFor="" className={`text-xs text-gray-500`}>
-            Address
-          </label>
-          <input
-            disabled={loading}
-            type="text"
-            placeholder={loading ? "please wait ...." : "Enter Your address"}
-            ref={inputRef}
-            className="block w-full rounded-md border-0 py-2 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            onKeyPress={(event) => {
-              if (event.key === "Enter") {
-                checkAvailability();
-              }
-            }}
-          />
-        </div>
-
-        <div className="w-full  mt-4">
-          <button
-            type="submit"
-            onClick={() => checkAvailability()}
-            className={`bg-primary border-none hover:bg-primary/80 rounded-md ${styles.paragraph} text-white px-4 py-2     w-full `}
-          >
-            Check availability
-          </button>
-        </div>
-      </>
-    );
-  }
+                <div className="w-full  mt-4">
+                    <button
+                        type="submit"
+                        onClick={() => checkAvailability()}
+                        className={`bg-primary border-none hover:bg-primary/80 rounded-md ${styles.paragraph} text-white px-4 py-2 w-full `}
+                    >
+                        Check availability
+                    </button>
+                </div>
+            </>
+        );
+    }
 };
 
 export default AutoCompleteInput;
