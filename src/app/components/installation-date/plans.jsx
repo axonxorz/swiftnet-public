8bdd252620edcea3e535b89f629ac8871d1e6186
Brendan Zerr <bzerr@swift-net.ca>
2023-10-18 12:47:43 -0600

    Remove BasicPlanCard and hardcoded plans

--- src/app/components/installation-date/plans.jsx
+++ src/app/components/installation-date/plans.jsx
@@ -1,35 +1,11 @@
 import styles from "@/app/styles/styles";
 import React, { useState } from "react";
 import Card from "./card";
-import BasicPlan from "./basic-plan-card";
+import { useAvailablePlansStore } from "@/store";
 
-export const plansList = [
-  {
-    id: 0,
-    title: "LightSpeed 25/5",
-    unlimited: true,
-    price: 79.95,
-    include: ["Youtube", "Netflix", "Live Sports"],
-    bg: "",
-  },
-  {
-    id: 1,
-    title: "LightSpeed 50/10",
-    unlimited: true,
-    price: 89.95,
-    include: ["Multiple users", "Online learning", "TikTok"],
-    bg: "",
-  },
-  {
-    id: 2,
-    title: "LightSpeed 75/15",
-    unlimited: true,
-    price: 99.95,
-    include: ["PC gaming", "Xbox", "PlayStation", "Nintendo"],
-    bg: "",
-  }
-];
 const Plans = ({ selectedPlan, setSelectedPlan }) => {
+  const availablePlansStore = useAvailablePlansStore();
+
   return (
     <div className="w-full text-start ">
       <label className={`${styles.paragraph} text-[#4B5563]`}>
@@ -38,14 +14,14 @@ const Plans = ({ selectedPlan, setSelectedPlan }) => {
 
       <div className="flex justify-start items-start gap-4 ">
         <div className="grid grid-cols-2 md:grid-cols-3  gap-4 w-full">
-          {plansList
+          {availablePlansStore.plans
             .map((plan) => {
               return (
                 <Card
+                  key={plan.id}
                   plan={plan}
                   selectedPlan={selectedPlan}
                   setSelectedPlan={setSelectedPlan}
-                  key={plan.id}
                 />
               );
             })}
