ecb98b76e354d8b0eaa3d7069c66473a02485027
Brendan Zerr <bzerr@terek.tech>
2023-11-03 16:28:28 -0600

    Remove Tarana special-case handler

--- src/app/api/prequalification/check/route.js
+++ src/app/api/prequalification/check/route.js
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 import { apiClient } from "@/lib/terek";
-import { filterTaranaPlans } from "@/lib/tarana-coverage";
 
 
 export async function POST(request) {
@@ -10,18 +9,6 @@ export async function POST(request) {
     try {
         const url = 'api/prequalification/check';
         const response = await apiClient.post(url, data);
-
-        // Special-case handling for Tarana plans. To be removed when Terek #682 is complete.
-        const plans = response.data.plans;
-        console.log(`${plans.length} plans:${JSON.stringify(plans, null, 2)}`);
-        if(plans.map((p) => p.name)
-            .some((name) => name.toLowerCase().includes('tarana'))) {
-            console.log('Returned plans contain a Tarana plan, attempting filtering');
-            response.data.plans = filterTaranaPlans(data.location.lat, data.location.lng, plans);
-            console.log(`${response.data.plans.length} plans after filtering`)
-        } else {
-            console.log('Returned plans do not contain Tarana plans');
-        }
 
         return NextResponse.json(response.data);
     } catch(error) {
