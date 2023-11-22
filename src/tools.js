141930d2038c2a4041fa827a4f91c871d6353859
Brendan Zerr <bzerr@terek.tech>
2023-11-22 15:39:32 -0600

    Don't fail if secure context is not available

--- src/tools.js
+++ src/tools.js
@@ -11,5 +11,10 @@ export async function postData(url = "", data = {}) {
 
 export const generateSessionId = () => {
   // Generate a unique session ID
-  return crypto.randomUUID();
+  if(!!crypto.randomUUID) {
+    return crypto.randomUUID();
+  } else {
+    console.warn('crypto.randomUUID not available. Not a secure context?')
+    return (Math.random() * 10000000).toString()
+  }
 };
