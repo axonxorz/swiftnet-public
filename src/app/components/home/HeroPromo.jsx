0cc433e447e9be5131549709f7a5bfe5d17bc7ca
Brendan Zerr <bzerr@terek.tech>
2024-01-08 15:36:45 -0600

    Disable promo notice

--- src/app/components/home/HeroPromo.jsx
+++ src/app/components/home/HeroPromo.jsx
@@ -3,7 +3,8 @@ import { DateTime } from "luxon";
 export default () => {
     const promoStarts = DateTime.local(2023, 12, 1, 0, 0)
     const promoEnds = DateTime.local(2024, 1, 31, 0, 0);
-    const promoActive = DateTime.now() >= promoStarts && DateTime.now() <= promoEnds;
+    // const promoActive = DateTime.now() >= promoStarts && DateTime.now() <= promoEnds;
+    const promoActive = false;
 
     return promoActive ? (
         <h3 className="font-semibold text-2xl md:text-2xl tracking-[-0.02em] text-white leading-[30px] backdrop-opacity-30 bg-neutral-900/50 p-3 pb-3 md:p-8 mb-[2rem]">
