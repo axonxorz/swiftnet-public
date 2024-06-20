17f670dc1cdc0ec87b7cd06ad2103c6e858b9544
Brendan Zerr <bzerr@terek.tech>
2024-06-20 16:11:44 -0600

    Test logging

--- src/app/api/outages/route.js
+++ src/app/api/outages/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import { apiClient } from "@/lib/terek";
   
-export async function GET(request) {
+export async function POST(request) {
     try {
         const url = 'api/outages/list';
         const params = {
