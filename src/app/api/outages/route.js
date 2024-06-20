37966c030ea476148e77c4b5172a63b58ca738f0
Brendan Zerr <bzerr@terek.tech>
2024-06-20 16:06:38 -0600

    Test logging

--- src/app/api/outages/route.js
+++ src/app/api/outages/route.js
@@ -7,18 +7,18 @@ export async function GET(request) {
         const params = {
             active: true
         }
-        console.debug(process.env.TEREK_BASE_URL);
-        console.debug('url', url);
+        console.log(process.env.TEREK_BASE_URL);
+        console.log('url', url);
         const response = await apiClient.get(url, {params});
-        console.debug('response', response);
+        console.log('response', response);
         const data = response.data;
-        console.debug('response.data', response.data);
+        console.log('response.data', response.data);
         if(!data.result) {
             console.log('error result');
             throw new Error(data);
         }
         const outages = data.objects;
-        console.debug('outages.length', outages.length)
+        console.log('outages.length', outages.length)
         return NextResponse.json(outages);
     } catch(error) {
         console.error('Error fetching outages', error);
