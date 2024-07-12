8722628333f7c3b76d20d5126200c643e7ecfe23
Dexter Uy <duy@swift-net.ca>
2024-07-12 13:09:30 -0600

    Add support for markdown formatting
    - Closes #117

--- src/app/components/home/HeroOutages.jsx
+++ src/app/components/home/HeroOutages.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { backendClient } from "@/lib/backend";
+import MarkdownView from "react-showdown";
 
 export default () => {
     const [outages, setOutages] = useState(null);
@@ -18,7 +19,7 @@ export default () => {
         <div key={outage.uuid}
              className={`md:w-[50%] mx-auto rounded p-3 font-bold`}
              style={{background: '#ffcc00', whiteSpace: 'pre-wrap'}}>
-            {outage.message}
+            <MarkdownView markdown={outage.message}/>              
         </div>
     ) : <></>
 }
