6fb95486fb38559e6d34a57b5114356d8edebdcb
Dexter Uy <duy@swift-net.ca>
2024-07-16 16:59:32 +0000

    Add support for markdown formatting

--- src/app/components/home/HeroOutages.jsx
+++ src/app/components/home/HeroOutages.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { backendClient } from "@/lib/backend";
+import MarkdownView from "react-showdown";
 
 export default () => {
-    const [outages, setOutages] = useState(null);
+    const [outages, setOutages] = useState([]);
 
     const getOutages = async () => {
         const outageUrl = '/api/outages'
@@ -14,11 +15,28 @@ export default () => {
         getOutages();
     }, []);
 
-    return (outages && outages?.length) ? outages.map((outage) =>
+    return <>
+        <style jsx global>{`
+            .outage {
+                font-size: 1em;
+                line-height: 1em;
+                h1, h2, h3, h4, h5 {
+                    font-size: 115%;
+                    font-weight: bold;
+                    line-height: 0.5em;
+                    margin-bottom: 0.25em;
+                }
+                a {
+                    color: rgb(48, 127, 228);
+                    text-decoration: underline;
+                }
+            }
+        `}</style>
+        {outages.map((outage) =>
         <div key={outage.uuid}
-             className={`md:w-[50%] mx-auto rounded p-3 font-bold`}
-             style={{background: '#ffcc00', whiteSpace: 'pre-wrap'}}>
-            {outage.message}
-        </div>
-    ) : <></>
+             className={`outage md:w-[50%] mx-auto rounded p-3`}
+             style={{background: '#ffbe0b', whiteSpace: 'pre-wrap'}}>
+            <MarkdownView markdown={outage.message}/>
+        </div>)}
+        </>
 }
