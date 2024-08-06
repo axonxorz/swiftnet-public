83136891a24ea90143b4183175b534adc153ddef
Brendan Zerr <bzerr@terek.tech>
2024-08-06 08:33:24 -0600

    Remove debugging statement

--- src/app/api/outages/route.js
+++ src/app/api/outages/route.js
@@ -18,11 +18,8 @@ export async function POST() {
         const params = {
             active: true
         }
-        console.log(`url=${process.env.TEREK_BASE_URL}/${url}`);
         const response = await apiClient.get(url, {params});
         const data = response.data;
-        console.log('response', response);
-        console.log('response.data', response.data);
         if(!data.result) {
             console.log('Error result');
             throw new Error(data);
